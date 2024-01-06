148c5a51c7a68caedeedbc752ab02b041a32c983
stella <sftamesis13@gmail.com>
2024-01-05 16:45:02 -0800

    Configure for Render

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # route to test your configuration
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
   get '/hello', to: 'application#hello_world'
+
 end
