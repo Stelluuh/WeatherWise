c67952dd5760fdb2d095e454f08da5819537c5a9
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-02-15 16:47:50 -0600

    adding in start of context file

---
+++ client/src/context/Context.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect } from  "react"
+import { useNavigate } from "react-router-dom"
+
+const AllContext = React.createContext()
+
+
+const AllContextProvider = ({ children }) => {
+    const navigate = useNavigate()
+    
+    return (
+        <AllContext.Provider>{children}</AllContext.Provider>
+    )
+}
+
+export {AllContext, AllContextProvider};
